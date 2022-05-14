3ed48fe8d99b21c77aca1fdb41f3bff8bd4ee41c
KodeKenobi <kodekenobi@gmail.com>
2022-05-14 11:57:08 +0200

    commit

---
+++ duplicate.py
@@ -0,0 +1,29 @@
+from tkinter.filedialog import askdirectory
+from tkinter import Tk
+import os
+import hashlib
+from pathlib import Path
+Tk().withdraw()
+path = askdirectory(title="Select A Folder")
+
+file_list = os.walk(path)
+
+unique = dict()
+for root, folders, files in file_list:
+    for file in files:
+        path = Path(os.path.join(root, file))
+        fileHash = hashlib.md5(open(path, 'rb').read()).hexdigest()
+        print(len(fileHash))
+        if fileHash not in unique:
+            unique[fileHash] = path
+
+        else:
+            os.walk(path)
+            print(f"{path} has been deleted")
+
+# IF YOU WANT TO DELETE DUPLICATES, UNCOMMENT THE CODE BELOW AND REPLACE THE ONE ON TOP BECAUSE
+# IT ONLY PRINTS OUT THE DUPLICATES AND DOES NOT DELETE
+#
+#        else:
+#            os.walk(path)
+#            print(f"{path} has been deleted")
